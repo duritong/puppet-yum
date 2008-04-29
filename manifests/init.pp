509961d1a332f113153cb95b80d2987cecfc0d76
mh <mh@immerda.ch>
2008-04-29 09:20:40 +0000

    reenabled repos :-/

--- manifests/init.pp
+++ manifests/init.pp
@@ -39,6 +39,7 @@ class yum::centos::five {
     yum::managed_yumrepo {base:
         descr => 'CentOS-$releasever - Base',
         mirrorlist => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=os',
+        enabled => 1,
         gpgcheck => 1,
         gpgkey => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-5',
         priority => 1,
@@ -47,6 +48,7 @@ class yum::centos::five {
     yum::managed_yumrepo {updates:
         descr => 'CentOS-$releasever - Updates',
         mirrorlist => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=updates',
+        enabled => 1,
         gpgcheck => 1,
         gpgkey => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-5',
         priority => 1,
@@ -55,6 +57,7 @@ class yum::centos::five {
     yum::managed_yumrepo {addons:
         descr => 'CentOS-$releasever - Addons',
         mirrorlist => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=addons',
+        enabled => 1,
         gpgcheck => 1,
         gpgkey => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-5',
         priority => 1,
@@ -63,6 +66,7 @@ class yum::centos::five {
     yum::managed_yumrepo {extras:
         descr => 'CentOS-$releasever - Extras',
         mirrorlist => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=extras',
+        enabled => 1,
         gpgcheck => 1,
         gpgkey => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-5',
         priority => 1,
@@ -72,6 +76,7 @@ class yum::centos::five {
     yum::managed_yumrepo {centosplus:
         descr => 'CentOS-$releasever - Centosplus',
         mirrorlist => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=centosplus',
+        enabled => 1,
         gpgcheck => 1,
         gpgkey => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-5',
         priority => 10,
@@ -80,6 +85,7 @@ class yum::centos::five {
     yum::managed_yumrepo {contrib:
         descr => 'CentOS-$releasever - Contrib',
         mirrorlist => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=contrib',
+        enabled => 1,
         gpgcheck => 1,
         gpgkey => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-5',
         priority => 10,
