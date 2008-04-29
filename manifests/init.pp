43453c062bc2f18199726a1b737b070377e4094a
mh <mh@immerda.ch>
2008-04-29 09:28:41 +0000

    temporarly disabled this repo

--- manifests/init.pp
+++ manifests/init.pp
@@ -119,7 +119,7 @@ class yum::centos::five {
 	yum::managed_yumrepo {centos5-atrpms:
 	    descr => 'CentOS $releasever - $basearch - ATrpms',
         baseurl => 'http://dl.atrpms.net/el$releasever-$basearch/atrpms/stable',
-	    enabled => 1,
+	    enabled => 0,
 		gpgcheck => 0,
     	gpgkey => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY.atrpms',
 	    priority => 30,
