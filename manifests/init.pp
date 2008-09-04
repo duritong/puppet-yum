d8c80fb96ae555e341eb30334e6dedbfefb7b856
mh <mh@immerda.ch>
2008-09-04 19:51:40 +0000

    added remi repo which seems to be managed

--- manifests/init.pp
+++ manifests/init.pp
@@ -235,6 +235,17 @@ class yum::centos::five {
 	}
 }
 
+class yum::remi {
+    yum::managed_yumrepo { remi:
+        descr => 'Les RPM de remi pour Enterpise Linux $releasever - $basearch',
+        baseurl => 'http://rpms.famillecollet.com/el$releasever.$basearch/',
+        enabled => 1,
+        gpgcheck => 1,
+        gpgkey => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-remi',
+        priority => 1,
+    }
+}
+
 class yum::prerequisites {
     package{yum-priorities:
         ensure => present,
