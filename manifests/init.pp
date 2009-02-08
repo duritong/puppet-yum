ebf414d0061c66f5415899633475b6a0f956e9cb
andreas <andreas@immerda.ch>
2009-02-08 17:36:11 +0000

    documentroot_recurse parameter for apache::vhost::webdir fcn

--- manifests/init.pp
+++ manifests/init.pp
@@ -345,3 +345,17 @@ define yum::managed_yumrepo (
         ],
     }    
 }
+
+# manifests/updatesd.pp
+
+class yum::updatesd {
+    package{'yum-updatesd':
+        ensure => present,
+    }
+
+    service{'yum-updatesd':
+        ensure => running,
+        enable => true,
+        require => Package['yum-updatesd'],
+    }
+}
