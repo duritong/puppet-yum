d342087b7f31b76b7c79f88d9310608e54f26b33
mh <mh@immerda.ch>
2009-10-31 16:41:46 +0100

    move the cron to z_ so it is always updated AFTER the possible daily um update

--- manifests/munin.pp
+++ manifests/munin.pp
@@ -1,22 +1,25 @@
 # manifests/yum.pp
 
 class yum::munin {
-    file{'/var/lib/munin/yum_updates.py':
-        source => "puppet://$server/yum/munin/yum_updates.py",
-        require => Package['munin-node'],
-        notify => Exec['yum_munin_updates_init'],
-        owner => root, group => 0, mode => 0755;
-    }
-    exec{'yum_munin_updates_init':
-        command => '/var/lib/munin/yum_updates.py',
-        refreshonly => true,
-    }
-    file{'/etc/cron.daily/munin_yum_updates.sh':
-        source => "puppet://$server/yum/munin/munin_yum_updates.sh",
-        require => File['/var/lib/munin/yum_updates.py'],
-        owner => root, group => 0, mode => 0755;
-    }
-    ::munin::plugin::deploy{'yum_updates':
-        source => 'yum/munin/yum_updates',
-    }
+  file{'/var/lib/munin/yum_updates.py':
+    source => "puppet://$server/yum/munin/yum_updates.py",
+    require => Package['munin-node'],
+    notify => Exec['yum_munin_updates_init'],
+    owner => root, group => 0, mode => 0755;
+  }
+  exec{'yum_munin_updates_init':
+    command => '/var/lib/munin/yum_updates.py',
+    refreshonly => true,
+  }
+  file{'/etc/cron.daily/munin_yum_updates.sh':
+    ensure => absent,
+  }
+  file{'/etc/cron.daily/z_munin_yum_updates.sh':
+    source => "puppet://$server/yum/munin/munin_yum_updates.sh",
+    require => File['/var/lib/munin/yum_updates.py'],
+    owner => root, group => 0, mode => 0755;
+  }
+  ::munin::plugin::deploy{'yum_updates':
+    source => 'yum/munin/yum_updates',
+  }
 }
