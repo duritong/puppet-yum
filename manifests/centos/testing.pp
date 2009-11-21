c74b52e2bed81bc9d5c5dae246ab9b8229f782de
mh <mh@immerda.ch>
2009-11-21 18:29:16 +0100

    add possibility to exclude packages for testing

--- manifests/centos/testing.pp
+++ manifests/centos/testing.pp
@@ -2,6 +2,9 @@ class yum::centos::testing {
     case $yum_centos_testing_include_pkgs {
       '': { fail('Please configure $yum_centos_testing_include_pkgs as we run the testing repo with highest repository') }
     }
+    case $yum_centos_testing_exclude_pkgs {
+      '': { $yum_centos_testing_exclude_pkgs = 'absent'
+    }
     yum::managed_yumrepo{'centos5-testing':
         descr => 'CentOS-$releasever - Testing',
         baseurl => 'http://dev.centos.org/centos/$releasever/testing/$basearch',
@@ -9,6 +12,7 @@ class yum::centos::testing {
         gpgcheck => 1,
         gpgkey => 'http://dev.centos.org/centos/RPM-GPG-KEY-CentOS-testing',
         priority => 1,
-        includepkgs => $yum_centos_testing_include_pkgs
+        includepkgs => $yum_centos_testing_include_pkgs,
+        exclude => $yum_centos_testing_exclude_pkgs,
     }
 }
