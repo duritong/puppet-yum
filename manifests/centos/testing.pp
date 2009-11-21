91d26005150681276b2c6509918063a811710d57
mh <mh@immerda.ch>
2009-11-21 18:19:01 +0100

    simplify testing

--- manifests/centos/testing.pp
+++ manifests/centos/testing.pp
@@ -1,4 +1,7 @@
 class yum::centos::testing {
+    case $yum_centos_testing_include_pkgs {
+      '': { fail('Please configure $yum_centos_testing_include_pkgs as we run the testing repo with highest repository') }
+    }
     yum::managed_yumrepo{'centos5-testing':
         descr => 'CentOS-$releasever - Testing',
         baseurl => 'http://dev.centos.org/centos/$releasever/testing/$basearch',
@@ -6,12 +9,6 @@ class yum::centos::testing {
         gpgcheck => 1,
         gpgkey => 'http://dev.centos.org/centos/RPM-GPG-KEY-CentOS-testing',
         priority => 1,
-    }
-    if $yum_centos_testing_include_pkgs {
-        Yum::Managed_yumrepo['centos5-testing']{
-          includepkgs => $yum_centos_testing_include_pkgs
-        }
-    } else {
-      fail('Please configure $yum_centos_testing_include_pkgs as we run the testing repo with highest repository')
+        includepkgs => $yum_centos_testing_include_pkgs
     }
 }
