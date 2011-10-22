e6c762de63856ebfd4f78e9d0b53bf86ed59a727
mh <mh@immerda.ch>
2011-10-22 11:58:53 +0200

    so this should make it as nearly as consistent as possible (with puppet) with upstream

--- manifests/centos/cr.pp
+++ manifests/centos/cr.pp
@@ -1,10 +1,14 @@
 class yum::centos::cr {
-    yum::managed_yumrepo{'cr':
-        descr => 'CentOS-$releasever - CR',
-        baseurl => 'http://mirror.centos.org/centos/$releasever/cr/$basearch/',
-        enabled => 1,
-        gpgcheck => 1,
-        gpgkey => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-${lsbmajdistrelease}",
-        priority => 1,
-    }
+  package{'centos-release-cr':
+    ensure => installed,
+  }
+  yum::managed_yumrepo{'CentOS-CR':
+    descr => 'CentOS-$releasever - CR',
+    baseurl => 'http://mirror.centos.org/centos/$releasever/cr/$basearch/',
+    enabled => 1,
+    gpgcheck => 1,
+    gpgkey => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-${lsbmajdistrelease}",
+    priority => 1,
+    require => Package['centos-release-cr'],
+  }
 }
