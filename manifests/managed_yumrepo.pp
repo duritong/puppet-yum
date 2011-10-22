b979d75b367926ef3d36e21ab5cccc5bd81a5d73
mh <mh@immerda.ch>
2011-10-22 11:48:26 +0200

    white space

--- manifests/managed_yumrepo.pp
+++ manifests/managed_yumrepo.pp
@@ -1,41 +1,42 @@
 define yum::managed_yumrepo (
-    $descr = 'absent',
-    $baseurl = 'absent',
-    $mirrorlist = 'absent',
-    $enabled = 0,
-    $gpgcheck = 0,
-    $gpgkey = 'absent',
-    $failovermethod = 'absent',
-    $priority = 99,
-    $exclude = 'absent',
-    $includepkgs = 'absent') {
+  $descr = 'absent',
+  $baseurl = 'absent',
+  $mirrorlist = 'absent',
+  $enabled = 0,
+  $gpgcheck = 0,
+  $gpgkey = 'absent',
+  $failovermethod = 'absent',
+  $priority = 99,
+  $exclude = 'absent',
+  $includepkgs = 'absent') {
 
-    # ensure that everything is setup
-    include ::yum::prerequisites
+  # ensure that everything is setup
+  include ::yum::prerequisites
 
-    file{"/etc/yum.repos.d/${name}.repo":
-        ensure => file,
-        replace => false,
-        before => Yumrepo[$name],
-        require => [ File[yum_repos_d],
-             Package[yum-priorities]
-        ],
-        mode => 0644, owner => root, group => 0;
-    }
+  file{"/etc/yum.repos.d/${name}.repo":
+    ensure => file,
+    replace => false,
+    before => Yumrepo[$name],
+    require => [ File[yum_repos_d],
+      Package[yum-priorities]
+    ],
+    mode => 0644, owner => root, group => 0;
+  }
 
-    yumrepo{$name:
-        descr => $descr,
-        baseurl => $baseurl,
-        mirrorlist => $mirrorlist,
-        enabled => $enabled,
-        gpgcheck => $gpgcheck,
-        gpgkey => $gpgkey,
-        failovermethod => $failovermethod,
-        priority => $priority,
-        exclude => $exclude,
-        includepkgs => $includepkgs,
-        require => [ File[rpm_gpg],
-            Package[yum-priorities]
-        ],
-    }
+  yumrepo{$name:
+    descr => $descr,
+    baseurl => $baseurl,
+    mirrorlist => $mirrorlist,
+    enabled => $enabled,
+    gpgcheck => $gpgcheck,
+    gpgkey => $gpgkey,
+    failovermethod => $failovermethod,
+    priority => $priority,
+    exclude => $exclude,
+    includepkgs => $includepkgs,
+    require => [
+      File[rpm_gpg],
+      Package[yum-priorities]
+    ],
+  }
 }
