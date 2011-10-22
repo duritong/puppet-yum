8f3a1ee6859e65d25ae537cbc48b223a716b5b4c
mh <mh@immerda.ch>
2011-10-22 11:44:38 +0200

    introduce centos CR repo

---
+++ manifests/centos/cr.pp
@@ -0,0 +1,10 @@
+class yum::centos::cr {
+    yum::managed_yumrepo{'cr':
+        descr => 'CentOS-$releasever - CR',
+        baseurl => 'http://mirror.centos.org/centos/$releasever/cr/$basearch/',
+        enabled => 1,
+        gpgcheck => 1,
+        gpgkey => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-${lsbmajdistrelease}",
+        priority => 1,
+    }
+}
