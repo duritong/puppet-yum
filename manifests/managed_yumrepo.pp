d03d23ad491f283e27dcf8b08313ff6a5dc0a710
mh <mh@immerda.ch>
2012-11-23 13:29:03 +0100

    solve a dependency cycle with stages with anchors

--- manifests/managed_yumrepo.pp
+++ manifests/managed_yumrepo.pp
@@ -1,35 +1,37 @@
 define yum::managed_yumrepo (
-  $descr = 'absent',
-  $baseurl = 'absent',
-  $mirrorlist = 'absent',
-  $enabled = 0,
-  $gpgcheck = 0,
-  $gpgkey = 'absent',
+  $descr          = 'absent',
+  $baseurl        = 'absent',
+  $mirrorlist     = 'absent',
+  $enabled        = 0,
+  $gpgcheck       = 0,
+  $gpgkey         = 'absent',
   $failovermethod = 'absent',
-  $priority = 99,
-  $exclude = 'absent',
-  $includepkgs = 'absent') {
+  $priority       = 99,
+  $exclude        = 'absent',
+  $includepkgs    = 'absent'
+) {
 
   # ensure that everything is setup
-  require ::yum::prerequisites
+  include ::yum::prerequisites
 
-  file{"/etc/yum.repos.d/${name}.repo":
-    ensure => file,
-    replace => false,
-    mode => 0644, owner => root, group => 0;
-  }
-
-  yumrepo{$name:
-    descr => $descr,
-    baseurl => $baseurl,
-    mirrorlist => $mirrorlist,
-    enabled => $enabled,
-    gpgcheck => $gpgcheck,
-    gpgkey => $gpgkey,
-    failovermethod => $failovermethod,
-    priority => $priority,
-    exclude => $exclude,
-    includepkgs => $includepkgs,
-    require => File["/etc/yum.repos.d/${name}.repo"];
+  Anchor['yum::prerequisites::done'] -> file{
+    "/etc/yum.repos.d/${name}.repo":
+      ensure  => file,
+      replace => false,
+      owner   => root,
+      group   => 0,
+      mode    => '0644';
+  } -> yumrepo{
+    $name:
+      descr           => $descr,
+      baseurl         => $baseurl,
+      mirrorlist      => $mirrorlist,
+      enabled         => $enabled,
+      gpgcheck        => $gpgcheck,
+      gpgkey          => $gpgkey,
+      failovermethod  => $failovermethod,
+      priority        => $priority,
+      exclude         => $exclude,
+      includepkgs     => $includepkgs;
   }
 }
--- manifests/prerequisites.pp
+++ manifests/prerequisites.pp
@@ -14,28 +14,33 @@ class yum::prerequisites {
   # ensure there are no other repos
   file {
     'yum_repos_d' :
-      path => '/etc/yum.repos.d/',
-      ensure => directory,
-      recurse => true,
-      purge => true,
-      force => true,
-      require => Package[yum-priorities],
-      mode => 0755,
-      owner => root,
-      group => 0 ;
+      path          => '/etc/yum.repos.d/',
+      ensure        => directory,
+      recurse       => true,
+      purge         => true,
+      force         => true,
+      require       => Package[yum-priorities],
+      before        => Anchor['yum::prerequisites::done'],
+      mode          => 0755,
+      owner         => root,
+      group         => 0 ;
 
     'rpm_gpg' :
-      path => '/etc/pki/rpm-gpg/',
-      source => [
+      path          => '/etc/pki/rpm-gpg/',
+      source        => [
         "puppet:///modules/yum/rpm-gpg/${::operatingsystem}.${::lsbmajdistrelease}/",
         "puppet:///modules/yum/rpm-gpg/default/"
       ],
-      sourceselect => all,
-      recurse => true,
-      purge => true,
-      force => true,
-      owner => root,
-      group => 0,
-      mode => '600' ;
+      sourceselect  => all,
+      require       => Package[yum-priorities],
+      before        => Anchor['yum::prerequisites::done'],
+      recurse       => true,
+      purge         => true,
+      force         => true,
+      owner         => root,
+      group         => 0,
+      mode          => '0600' ;
   }
+
+  anchor{'yum::prerequisites::done': }
 }
