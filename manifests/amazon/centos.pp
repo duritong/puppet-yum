49d7b13a570453c0a37f4da49a11a90f19cd73ca
Daniel Zuberbühler <dani@zubinet.org>
2013-01-18 17:40:24 +0100

    enable centos repos for amazon

--- manifests/amazon/centos.pp
+++ manifests/amazon/centos.pp
@@ -3,21 +3,21 @@ class yum::amazon::centos {
         'centos-base':
           descr => 'CentOS-6 - Base',
           mirrorlist => 'http://mirrorlist.centos.org/?release=6&arch=$basearch&repo=os',
-          enabled => 0,
+          enabled => 1,
           gpgcheck => 1,
           gpgkey => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6',
           priority => 50;
         'centos-updates':
           descr => 'CentOS-6 - Updates',
           mirrorlist => 'http://mirrorlist.centos.org/?release=6&arch=$basearch&repo=updates',
-          enabled => 0,
+          enabled => 1,
           gpgcheck => 1,
           gpgkey => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6',
           priority => 50;
         'centos-extras':
           descr => 'CentOS-6 - Extras',
           mirrorlist => 'http://mirrorlist.centos.org/?release=6&arch=$basearch&repo=extras',
-          enabled => 0,
+          enabled => 1,
           gpgcheck => 1,
           gpgkey => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6',
           priority => 50;
