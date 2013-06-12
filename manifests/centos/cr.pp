3a97ffce07d93681355cf91d72f7720e7a84a115
mh <mh@immerda.ch>
2013-06-12 18:38:23 +0200

    this needs the extras repo

--- manifests/centos/cr.pp
+++ manifests/centos/cr.pp
@@ -1,14 +1,14 @@
+# centos rolling repo
 class yum::centos::cr {
   package{'centos-release-cr':
-    ensure => installed,
-  }
-  yum::managed_yumrepo{'CentOS-CR':
-    descr => 'CentOS-$releasever - CR',
-    baseurl => 'http://mirror.centos.org/centos/$releasever/cr/$basearch/',
-    enabled => 1,
+    ensure  => installed,
+    require => Yum::Managed_yumrepo['extras'];
+  } -> yum::managed_yumrepo{'CentOS-CR':
+    descr    => 'CentOS-$releasever - CR',
+    baseurl  => 'http://mirror.centos.org/centos/$releasever/cr/$basearch/',
+    enabled  => 1,
     gpgcheck => 1,
-    gpgkey => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-${::lsbmajdistrelease}",
+    gpgkey   => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-${::lsbmajdistrelease}",
     priority => 1,
-    require => Package['centos-release-cr'],
   }
 }
