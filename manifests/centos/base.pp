d375db91bfcf0ed6c3ee2d9ec49f5b1cc1b6f737
mh <mh@immerda.ch>
2014-07-11 18:14:35 +0200

    these repos are empty anyways

--- manifests/centos/base.pp
+++ manifests/centos/base.pp
@@ -104,35 +104,6 @@ class yum::centos::base {
       failovermethod => priority,
       gpgkey         => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL',
       priority       => 17;
-    'kbs-CentOS-Extras':
-      descr          => 'CentOS.Karan.Org-EL$releasever - Stable',
-      baseurl        => 'http://centos.karan.org/el$releasever/extras/stable/$basearch/RPMS/',
-      enabled        => 1,
-      gpgcheck       => 1,
-      gpgkey         => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-kbsingh',
-      priority       => 20;
-    'kbs-CentOS-Extras-Testing':
-      descr          => 'CentOS.Karan.Org-EL$releasever - Testing',
-      baseurl        => 'http://centos.karan.org/el$releasever/extras/testing/$basearch/RPMS/',
-      enabled        => 0,
-      gpgcheck       => 1,
-      gpgkey         => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-kbsingh',
-      priority       => 20;
-    'kbs-CentOS-Misc':
-      descr          => 'CentOS.Karan.Org-EL$releasever - Stable',
-      baseurl        => 'http://centos.karan.org/el$releasever/misc/stable/$basearch/RPMS/',
-      enabled        => 1,
-      gpgcheck       => 1,
-      gpgkey         => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-kbsingh',
-      priority       => 20;
-    'kbs-CentOS-Misc-Testing':
-      descr          => 'CentOS.Karan.Org-EL$releasever - Testing',
-      baseurl        => 'http://centos.karan.org/el$releasever/misc/testing/$basearch/RPMS/',
-      enabled        => 0,
-      gpgcheck       => 1,
-      gpgkey         => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-kbsingh',
-      priority       => 20;
-
     'elrepo':
       descr          => 'ELRepo.org Community Enterprise Linux Repository - el$releasever',
       baseurl        => 'http://elrepo.org/linux/elrepo/el$relesever/$basearch/',
