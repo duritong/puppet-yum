e37593a3c0f6bc9fe941ccf18698773075879e0b
mh <mh@immerda.ch>
2019-07-26 14:51:38 +0200

    puppet 4 is long gone

--- manifests/centos/puppet.pp
+++ manifests/centos/puppet.pp
@@ -9,69 +9,36 @@
 class yum::centos::puppet(
   $repos = {},
 ){
-  if versioncmp($puppetversion,'4.0') >= 0 {
-    package{'puppet-release':
-      ensure => 'installed',
-    }
-    $release = $facts['os']['release']['major']
-    $default_repos = {
-      'puppet'        => {
-        descr         => "Puppet Repository el ${release} - \$basearch",
-        baseurl       => "http://yum.puppetlabs.com/puppet/el/${release}/\$basearch",
-        gpgkey        => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-puppet-release',
-        gpgkey_source => 'puppet:///modules/yum/rpm-gpg/additional/RPM-GPG-KEY-puppet-release',
-        gpgkeyid      => 'EF8D349F',
-        enabled       => 1,
-        gpgcheck      => 1,
-        priority      => 1,
-      },
-      'puppet-source' => {
-        descr         => "Puppet Repository el ${release} - Source",
-        baseurl       => "http://yum.puppetlabs.com/puppet/el/${release}/SRPMS",
-        gpgkey        => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-puppet-release',
-        manage_gpgkey => false,
-        enabled       => 0,
-        gpgcheck      => 1,
-        priority      => 1,
-      },
-    }
-  } else {
-    $default_repos = {
-      'puppet' => {
-        descr         => 'Puppet products',
-        baseurl       => 'http://yum.puppet.com/el/$releasever/products/$basearch/',
-        enabled       => 1,
-        gpgcheck      => 1,
-        gpgkey        => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-puppet-release',
-        gpgkey_source => 'puppet:///modules/yum/rpm-gpg/additional/RPM-GPG-KEY-puppet-release',
-        gpgkeyid      => 'EF8D349F',
-        priority      => 1,
-      },
-      'puppet-dependencies' => {
-        descr         => 'Puppetlabs dependencies',
-        baseurl       => 'http://yum.puppet.com/el/$releasever/dependencies/$basearch/',
-        enabled       => 1,
-        gpgcheck      => 1,
-        gpgkey        => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-puppet-release',
-        manage_gpgkey => false,
-        priority      => 1,
-      },
-      'puppet-devel' => {
-        descr         => 'Puppetlabs devel deps',
-        baseurl       => 'http://yum.puppet.com/el/$releasever/devel/$basearch/',
-        enabled       => 0,
-        gpgcheck      => 1,
-        gpgkey        => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-puppet-release',
-        manage_gpgkey => false,
-        priority      => 1,
-      },
-    }
+  package{'puppet-release':
+    ensure => 'installed',
+  }
+  $release = $facts['os']['release']['major']
+  $default_repos = {
+    'puppet'        => {
+      descr         => "Puppet Repository el ${release} - \$basearch",
+      baseurl       => "http://yum.puppetlabs.com/puppet/el/${release}/\$basearch",
+      gpgkey        => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-puppet-release',
+      gpgkey_source => 'puppet:///modules/yum/rpm-gpg/additional/RPM-GPG-KEY-puppet-release',
+      gpgkeyid      => 'EF8D349F',
+      enabled       => 1,
+      gpgcheck      => 1,
+      priority      => 1,
+    },
+    'puppet-source' => {
+      descr         => "Puppet Repository el ${release} - Source",
+      baseurl       => "http://yum.puppetlabs.com/puppet/el/${release}/SRPMS",
+      gpgkey        => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-puppet-release',
+      manage_gpgkey => false,
+      enabled       => 0,
+      gpgcheck      => 1,
+      priority      => 1,
+    },
   }
   $all_repos = deep_merge($default_repos,$repos)
   $all_repos.each |$repo,$params| {
     yum::repo{
       $repo:
-        * => $params
+        * => $params,
     }
   }
 }
