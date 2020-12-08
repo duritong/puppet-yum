66e7d34142e97108ab0784d7c0049ac77a27f3cd
mh <mh@immerda.ch>
2020-12-08 17:13:55 +0100

    all goes to stream now

--- manifests/centos/cr.pp
+++ manifests/centos/cr.pp
@@ -1,5 +1,6 @@
 # centos rolling repo
 class yum::centos::cr {
+  # there is no CR from streams
   if versioncmp($facts['os']['release']['major'],'8') < 0 {
     yum::repo{'CentOS-CR':
       descr         => 'CentOS-$releasever - CR',
@@ -8,16 +9,6 @@ class yum::centos::cr {
       gpgcheck      => 1,
       repo_gpgcheck => 1,
       gpgkey        => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-${::operatingsystemmajrelease}",
-      priority      => 1,
-    }
-  } else {
-    yum::repo{'cr':
-      descr         => 'CentOS-$releasever - cr',
-      mirrorlist    => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=cr&infra=$infra',
-      gpgcheck      => 1,
-      repo_gpgcheck => 1,
-      enabled       => 1,
-      gpgkey        => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-centosofficial',
       priority      => 1,
     }
   }
