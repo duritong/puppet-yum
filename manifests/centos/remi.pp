c851a3e340b83ad86f91139a0b8924bb23671725
o <o@immerda.ch>
2021-10-03 21:30:24 +0200

    fix remi repo on centos 8

--- manifests/centos/remi.pp
+++ manifests/centos/remi.pp
@@ -10,14 +10,23 @@ class yum::centos::remi(
   $repos = {},
 ){
   $release = $facts['os']['release']['major']
+  $mirrorlist = versioncmp($release, '8') ? {
+    -1      => "http://cdn.remirepo.net/enterprise/${release}/safe/mirror",
+    default => "http://cdn.remirepo.net/enterprise/${release}/safe/\$basearch/mirror"
+  }
+  $keyid = $release ? {
+    '7' => '00F97F56',
+    '8' => '555097595F11735A',
+  }
+
   $default_repos = {
     'remi-safe'     => {
       descr         => "Safe Remi's RPM repository for Enterprise Linux ${release} - \$basearch",
-      mirrorlist    => "http://cdn.remirepo.net/enterprise/${release}/safe/mirror",
+      mirrorlist    => $mirrorlist,
       gpgkey        => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-remi',
-      gpgkey_source => 'puppet:///modules/yum/rpm-gpg/additional/RPM-GPG-KEY-remi',
-      gpgkeyid      => '00F97F56',
+      gpgkey_source => "puppet:///modules/yum/rpm-gpg/additional/RPM-GPG-KEY-remi.el$release",
       enabled       => 1,
+      gpgkeyid      => $keyid,
       gpgcheck      => 1,
       priority      => 1,
     },
@@ -25,7 +34,7 @@ class yum::centos::remi(
       descr         => "Safe Remi's RPM repository for Enterprise Linux ${release} - \$basearch - debuginfo",
       baseurl       => "http://rpms.remirepo.net/enterprise/${release}/debug-remi/\$basearch/",
       gpgkey        => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-remi',
-      gpgkey_source => 'puppet:///modules/yum/rpm-gpg/additional/RPM-GPG-KEY-remi',
+      gpgkey_source => "puppet:///modules/yum/rpm-gpg/additional/RPM-GPG-KEY-remi.el$release",
       manage_gpgkey => false,
       enabled       => 0,
       gpgcheck      => 1,
