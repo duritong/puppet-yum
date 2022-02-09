31fcef7f3e8a1713a6b43018d78f79cadcfc5299
mh <mh@immerda.ch>
2022-02-09 22:07:27 +0100

    this is migrated

--- manifests/centos/puppet.pp
+++ manifests/centos/puppet.pp
@@ -9,21 +9,7 @@
 class yum::centos::puppet (
   $repos = {},
 ) {
-  file { '/etc/pki/rpm-gpg/GPG-KEY-puppet-20210817':
-    source => 'puppet:///modules/yum/rpm-gpg/additional/RPM-GPG-KEY-puppet-release-20210817',
-    owner  => root,
-    group  => root,
-    mode   => '0644',
-  } ~> exec { 'validate RPM-GPG-KEY-puppet-release-20210817':
-      command     => 'gpg --keyid-format 0xLONG /etc/pki/rpm-gpg/GPG-KEY-puppet-20210817 | grep -q 0x7F438280EF8D349F',
-      refreshonly => true,
-      logoutput   => 'on_failure',
-  } ~> exec { 'import RPM-GPG-KEY-puppet-release-20210817':
-      command     => 'rpm --import /etc/pki/rpm-gpg/GPG-KEY-puppet-20210817',
-      unless      => 'rpm -q gpg-pubkey-`echo $(gpg --throw-keyids < /etc/pki/rpm-gpg/GPG-KEY-puppet-20210817) | cut --characters=11-18 | tr [A-Z] [a-z]`',
-      refreshonly => true,
-      logoutput   => 'on_failure',
-  } -> package { 'puppet-release':
+  package { 'puppet-release':
     ensure => 'installed',
   }
   $release = $facts['os']['release']['major']
